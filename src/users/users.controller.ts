87c4b71b399c6785f8ce448c16c9dfe15cd195fa
ddong180302 <trandangdong18032002@gmail.com>
2023-12-28 12:56:41 +0700

    create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,11 +5,14 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Body('name') name: string) {
+    return this.usersService.create(email, password, name);
   }
 
   @Get()
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User, UserSchema } from './schemas/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
